babb05d193e3839aea68203ec427286c42d12afa
Nicolas Sabbatini <nik.code.things@gmail.com>
2023-09-27 19:31:27 -0300

    Part 2

--- cmd/quizes/quizes.go
+++ cmd/quizes/quizes.go
@@ -6,8 +6,10 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"math/rand"
 	"os"
 	"strings"
+	"time"
 )
 
 type Question struct {
@@ -26,9 +28,11 @@ func (q *Question) isCorrect(answer string) bool {
 type Quiz struct {
 	questions []Question
 	correct   int
+	time      time.Duration
+	finished  bool
 }
 
-func (q *Quiz) askQuiz() {
+func (q *Quiz) askQuiz(done chan bool) {
 	for number, question := range q.questions {
 		fmt.Println("\nQuestion #", number+1)
 		question.ask()
@@ -40,6 +44,24 @@ func (q *Quiz) askQuiz() {
 		}
 		fmt.Println("You have", q.correct, "correct answers")
 	}
+	q.finished = true
+	done <- true
+}
+
+// func (q *Quiz) startTimer(done chan bool) {
+// 	time.Sleep(q.time)
+// 	done <- true
+//}
+
+func (q *Quiz) startTimer() <-chan time.Time {
+	return time.NewTimer(q.time).C
+}
+
+func (q *Quiz) finishGame() {
+	if !q.finished {
+		fmt.Println("🕑 Time is up! 🕑 Better luck next time!")
+	}
+	q.finished = true
 	fmt.Println("\nYou have", q.correct, "correct answers out of", len(q.questions), "questions")
 	if q.correct == len(q.questions) {
 		fmt.Println("🎉🎉🎉 Congratulations you are a GENIUS 🎉🎉🎉")
@@ -72,15 +94,20 @@ func loadQuiz(path string) Quiz {
 	csvRows, err := csv.NewReader(quizCsv).ReadAll()
 	isFatalError("Error reading CSV file", err)
 	var questions []Question
+	duration, err := time.ParseDuration(csvRows[0][1])
+	isFatalError("Error parsing duration", err)
 	for _, row := range csvRows[1:] {
 		questions = append(questions, Question{
 			question: row[0],
 			correct:  row[1],
 		})
 	}
+	rand.Shuffle(len(questions), func(i, j int) { questions[i], questions[j] = questions[j], questions[i] })
 	quiz := Quiz{
 		questions: questions,
 		correct:   0,
+		time:      duration,
+		finished:  false,
 	}
 	return quiz
 }
@@ -98,5 +125,13 @@ func awaitAnswer() string {
 
 func main() {
 	quiz := loadQuiz(parseArgs())
-	quiz.askQuiz()
+	done := make(chan bool)
+	go quiz.askQuiz(done)
+	// go quiz.startTimer()
+	timer := quiz.startTimer()
+	select {
+	case <-timer:
+	case <-done:
+	}
+	quiz.finishGame()
 }
